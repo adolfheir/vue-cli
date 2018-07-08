0d9f14eb931a2af5a6a0d2a07efc3556d04cb6ea
adolfheir <1428899467@qq.com>
2018-07-08 17:22:53 +0800

    init

---
+++ src/views/form/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app-container">
+    formPage1
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+.line {
+  text-align: center;
+}
+</style>
+
---
+++ src/views/table/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app-container">
+    tablePage
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      list: null,
+      listLoading: true
+    }
+  },
+  filters: {
+  },
+  created() {
+  },
+  methods: {
+
+  }
+}
+</script>
